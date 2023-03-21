796348d450ee5a651ed89d0bfadf7ce98c5d53d6
David Ewan Campbell <ewan.david@gmail.com>
2023-03-21 17:30:32 +0000

    extends React App class added to replace App function

--- src/index.js
+++ src/index.js
@@ -9,11 +9,20 @@ const App = () => {
   );
   return(
     <div>
-      Hello World!
+      latitude: 
     </div>
   )
 }
 
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        You are in the Northern Hemisphere!
+      </div>
+    )
+  }
+}
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
