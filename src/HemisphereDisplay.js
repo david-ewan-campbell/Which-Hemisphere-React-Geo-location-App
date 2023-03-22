bd429cf268e4ad3aa031c096aebffa9af5beac9d
David Ewan Campbell <ewan.david@gmail.com>
2023-03-22 17:40:23 +0000

    Images folder added with images of each hemisphere to be called in the Hemisphere display component

--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import northernHemisphere from './images/northern-hemisphere.png';
+import southernHemisphere from './images/southern-hemisphere.png';
 
 const HemisphereDisplay = ( { latitude} ) => {
-  console.log(latitude)
+
+  const hemisphere = latitude > 0 ? 'You are in the Northern Hemisphere' : 'You are in the Southern Hemisphere';
+  const picture = latitude > 0 ? northernHemisphere : southernHemisphere;
   return(
     <div>
-      You are in the Northern Hemisphere!
+      <img src={ picture } alt=""/>
+      { hemisphere }
     </div>
   )
 }
